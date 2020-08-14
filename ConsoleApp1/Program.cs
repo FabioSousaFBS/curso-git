52950162ff6c8a85c760c88cb94b2af1cc19881c
Fabio <fabio.sousa28@outlook.com>
2020-08-13 23:39:18 -0300

    Acrescentada a mensagem Desejando um bom dia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,9 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Good mornning!");
-
+            Console.WriteLine("Hello World!");            
             Console.WriteLine("How are you today?");
+            Console.WriteLine("Have a good day!");
 
             Console.ReadKey();
 
